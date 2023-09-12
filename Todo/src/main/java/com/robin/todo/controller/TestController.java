b479509529324b1882332c7e84ad973fdb569ae7
Boruzs Robin <boruzsrobin@gmail.com>
2023-09-12 16:16:58 +0200

    refactor: authentication implemented properly

--- Todo/src/main/java/com/robin/todo/controller/TestController.java
+++ Todo/src/main/java/com/robin/todo/controller/TestController.java
@@ -1,17 +1,13 @@
 package com.robin.todo.controller;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+//Leaving it for later testing
+@RestController
 public class TestController {
 
-    @GetMapping("/all")
-    public ResponseEntity<?> allAccess() {
-        return ResponseEntity.ok("All access");
-    }
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER')")
